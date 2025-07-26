dc15bfe01d2ab021ed8703bec67183db776529d9
ravindu644 <droidcasts@protonmail.com>
2025-07-26 20:44:06 +0530

    fixup(wlan-adaptor): Hardcode build for wmt_chrdev_wifi
    OEM MODULE NAME = wmt_chrdev_wifi.ko
    
    The original, complex Makefile for the WLAN adaptor was designed to build multiple targets (connac1x, connac2x, etc.) based on variables passed from a higher-level build system.
    
    To simplify integration into the kernel's Kbuild system, this commit modifies the local Makefiles to build only the required `connac1x` variant.
    
    The top-level `Makefile` is now a simple wrapper that calls the build process for the `build/connac1x` subdirectory, and the `Kbuild` file is modified to use the hardcoded module name `wmt_chrdev_wifi` and `CONNAC_VER=1_0`. This ensures a predictable and clean build of the single required module.

--- vendor/mediatek/kernel_modules/connectivity/wlan/adaptor/build_cdev_wifi.mk
+++ vendor/mediatek/kernel_modules/connectivity/wlan/adaptor/build_cdev_wifi.mk
@@ -1,18 +1,11 @@
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := $(WIFI_NAME).ko
+LOCAL_MODULE := wmt_chrdev_wifi.ko
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_OWNER := mtk
 LOCAL_INIT_RC := init.wlan_drv.rc
+LOCAL_REQUIRED_MODULES := wmt_drv.ko
 
-ifeq ($(CONNAC_VER), 3_0)
-
-else ifeq ($(CONNAC_VER), 2_0)
-    LOCAL_REQUIRED_MODULES := conninfra.ko
-else
-    LOCAL_REQUIRED_MODULES := wmt_drv.ko
-endif
-
-WIFI_ADAPTOR_OPTS := CONNAC_VER=$(CONNAC_VER) MODULE_NAME=$(WIFI_NAME)
+WIFI_ADAPTOR_OPTS := CONNAC_VER=1_0 MODULE_NAME=wmt_chrdev_wifi
 include $(MTK_KERNEL_MODULE)
 $(linked_module): OPTS += $(WIFI_ADAPTOR_OPTS)
