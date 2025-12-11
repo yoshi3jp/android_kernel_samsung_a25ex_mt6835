f8bfc17986e3c36bc280b8a3e1e4b1477b3159e2
ravindu644 <droidcasts@protonmail.com>
2025-12-11 13:17:41 +0530

    build script: export common environment variables from Samsung's build_kernel.sh
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- build.sh
+++ build.sh
@@ -40,4 +40,28 @@ install_requirements() {
     fi
 }
 
+export_common_build_env() {
+
+    cd ${SCRIPT_DIR}/kernel-5.15
+
+    # cook build config
+    python scripts/gen_build_config.py \
+        --kernel-defconfig a16xm_00_defconfig \
+        --kernel-defconfig-overlays "entry_level.config S98901AA1.config S98901AA1_debug.config" \
+        -m user -o ../out/target/product/a16xm/obj/KERNEL_OBJ/build.config &>/dev/null
+
+    # common exports from samsung's build_kernel.sh
+    export ARCH=arm64
+    export PLATFORM_VERSION=13
+    export CROSS_COMPILE="aarch64-linux-gnu-"
+    export CROSS_COMPILE_COMPAT="arm-linux-gnueabi-"
+    export OUT_DIR="../out/target/product/a16xm/obj/KERNEL_OBJ"
+    export DIST_DIR="../out/target/product/a16xm/obj/KERNEL_OBJ"
+    export BUILD_CONFIG="../out/target/product/a16xm/obj/KERNEL_OBJ/build.config"
+
+    cd ${SCRIPT_DIR}
+
+}
+
 install_requirements
+export_common_build_env
